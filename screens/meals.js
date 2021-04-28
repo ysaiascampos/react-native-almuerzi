e110a3dcab5d004d1c3161069144b142c5b70090
ysaiascampos <ysaiascampos@gmail.com>
2021-04-27 21:57:40 -0400

    use fetch

--- screens/meals.js
+++ screens/meals.js
@@ -1,10 +1,57 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View,Text, StyleSheet, FlatList } from 'react-native';
+import ListItem from '../components/ListItem';
 
-export default () => {
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'flex-start',
+        justifyContent: 'flex-start',
+    },
+    list: {
+        alignSelf: 'stretch',
+    }
+})
+
+
+const Meals = ({ navigation }) => {
+    const [loading, setLoading] = useState(true);
+    const [meals, setMeals] = useState([]);
+
+    const fetchMeals = async () => {
+        const response = await fetch('https://server-less-native-k9b1tdp6k-ysaiascampos.vercel.app/api/meals/');
+        const data =await response.json();
+        setMeals(data);
+        setLoading(false)
+    }
+
+    useEffect(() => {
+        fetchMeals();
+    },[])
+    
     return (
-        <View>
-            <Text>Soy meals</Text>
+        <View style={styles.container}>
+            {loading ? <Text>Cargando...</Text>:
+                <FlatList
+                    style={styles.list}
+                    data={meals}
+                    keyExtractor={x => x._id}
+                    renderItem={({ item }) =>
+                        <ListItem
+                            onPress={() => navigation.navigate('Modal', { _id: item._id})}
+                            name={item.name}
+                        />
+                    }
+                />
+            }
+            
         </View>
     )
 }
+
+Meals.navigationOptions = ({
+    title: 'Comidas disponibles',
+})
+
+export default Meals

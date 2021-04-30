aeee4d44a27d47245a719cd44b377e4c57ee6944
ysaiascampos <ysaiascampos@gmail.com>
2021-04-29 21:52:33 -0400

    orders

--- screens/modal.js
+++ screens/modal.js
@@ -1,10 +1,43 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet, Button } from 'react-native';
+import useFetch from '../hooks/useFetch';
 
-export default () => {
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    }
+})
+export default ({ navigation }) => {
+    const id = navigation.getParam('_id')
+    const { loading, data } = useFetch(`https://server-less-native-k9b1tdp6k-ysaiascampos.vercel.app/api/meals/${id}`);
     return (
-        <View>
-            <Text>Soy modal</Text>
+        
+        <View style={styles.container}>
+            {loading? <Text>Cargando...</Text> :
+            <>
+                <Text>{data._id}</Text>
+                <Text>{data.name}</Text>
+                <Text>{data.desc}</Text>
+                <Button title="Aceptar" onPress={()=>{
+                    fetch('https://server-less-native-k9b1tdp6k-ysaiascampos.vercel.app/api/orders',{
+                        method: 'POST',
+                        headers: {
+                            'Content-Type': 'application/json'
+                        },
+                        body: JSON.stringify({
+                            meal_id: id,
+                            user_id: 'lalala',
+                        })
+                    }).then(()=> {
+                        alert('Orden fue generada con exito')
+                        navigation.navigate('Meals')
+                    })
+                }} />
+                <Button title="Cancelar" onPress={() => navigation.navigate('Meals')} />
+            </>
+            }
         </View>
     )
 }
